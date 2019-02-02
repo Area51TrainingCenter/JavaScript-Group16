29ff4667b5d634d40652e1bf58310bfceb79a65c
Renzo Castro <renzo.vista@gmail.com>
2019-02-02 12:41:04 -0500

    Clase 3: DOM eventos

--- clase-3/dom/main.js
+++ clase-3/dom/main.js
@@ -1,5 +1,6 @@
 let titulo = document.createElement('h1');
 titulo.textContent = 'Título';
+titulo.classList.add('has-transition');
 
 document.body.appendChild(titulo);
 
@@ -14,34 +15,52 @@ document.body.appendChild(lista);
 
 
 
-let persona1 = document.createElement('li');
-persona1.textContent = 'Lucho';
-persona1.classList.add('persona');
+// let persona1 = document.createElement('li');
+// persona1.textContent = 'Lucho';
+// persona1.classList.add('persona');
 
-lista.appendChild(persona1);
-
-
-let persona2 = document.createElement('li');
-persona2.textContent = 'Jessica';
-persona2.classList.add('persona');
-
-lista.appendChild(persona2);
+// lista.appendChild(persona1);
 
 
-let persona3 = document.createElement('li');
-persona3.textContent = 'Carla';
-persona3.classList.add('persona');
+// let persona2 = document.createElement('li');
+// persona2.textContent = 'Jessica';
+// persona2.classList.add('persona');
 
-lista.appendChild(persona3);
-
-
+// lista.appendChild(persona2);
 
 
-/*
+// let persona3 = document.createElement('li');
+// persona3.textContent = 'Carla';
+// persona3.classList.add('persona');
+
+// lista.appendChild(persona3);
+
+
+
+
+
 let personas = ['Carla','Jessica','Diego'];
+
+const crearPersona = nombre => {
+  let persona = document.createElement('li');
+  persona.textContent = nombre;
+  persona.classList.add('persona');
+
+  lista.appendChild(persona);
+};
+
+const crearPersonas = listaPersonas => {
+  listaPersonas.forEach(crearPersona);
+};
+
 
 crearPersonas(personas);
 
-  crearPersona('Carla');
-*/
+  // crearPersona('Carla');
 
+
+let btn = document.getElementById('miBoton');
+
+btn.addEventListener('click', evento => {
+  titulo.classList.toggle('error');
+});
